4ee5d0034f86c3472fc470088dd034333309b208
Bhuwan-Shahi <shahibhuwan265@gmail.com>
2025-06-17 07:41:57 +0545

    configt & models

--- internal/config/config.go
+++ internal/config/config.go
@@ -1,1 +1,25 @@
 package config
+
+import "os"
+
+type Config struct {
+	Port     string
+	DataPath string
+}
+
+func Load() *Config {
+	port := os.Getenv("PORT")
+
+	if port == "" {
+		port = "8080" // Default port
+	}
+	dataPath := os.Getenv("DATA_PATH")
+	if dataPath == "" {
+		dataPath = "data" // Default data path
+	}
+
+	return &Config{
+		Port:     port,
+		DataPath: dataPath,
+	}
+}
--- internal/models/models.go
+++ internal/models/models.go
@@ -1,1 +1,37 @@
 package models
+
+type WordData struct {
+	Adjectives string `json:"adjectives"`
+	Adverbs    string `json:"adverbs"`
+	Nouns      string `json:"nouns"`
+	Verbs      string `json:"verbs"`
+}
+
+func (w *WordData) TotalCount() int {
+	return len(w.Adjectives) + len(w.Adverbs) + len(w.Nouns) + len(w.Verbs)
+}
+
+type ParagraphRequest struct {
+	Sentences int `json:"adjectives"`
+	Style     int `json:"adverbs"`
+	Topic     int `json:"nouns"`
+}
+
+type ParagraphResponse struct {
+	Paragraph string `json:"paragraph"`
+	WordCount int    `json:"word_count"`
+	Sentences int    `json:"sentences"`
+}
+
+type ErrorResponse struct {
+	Message string `json:"error"`
+}
+
+type InfoResponse struct {
+	Name        int               `json:"name"`
+	Version     int               `json:"version"`
+	Description string            `json:"description"`
+	Endpoints   map[string]string `json:"endpoints"`
+	Parameters  map[string]string `json:"parameters"`
+	Examples    map[string]string `json:"examples"`
+}
